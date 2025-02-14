ac8bd6edacb1963588911a117015e35a4c051278
Nishath-Mumtahin <mumtahinnishath@gmail.com>
2025-02-14 08:30:15 +0600

    added free drinks

---
+++ Free Drinks.js
@@ -0,0 +1,15 @@
+/***
+
+Free Drinks
+    - Burger more than 500tk: free Coke
+    - Else Coke: 30tk
+*/
+const customerTaka = 700;
+
+if(customerTaka > 500){
+    console.log('Burger with free Coke')
+}
+
+else{
+    console.log('Coke: 30tk')
+}
